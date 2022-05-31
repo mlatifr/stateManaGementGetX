40e75f34d09a8be3b65bcfb6b585399088586808
mlatifr <mlatifraldi@gmail.com>
2022-06-01 00:26:27 +0700

    getX getBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: GetX<CounterController>(
+        child: GetBuilder<CounterController>(
           init: CounterController(),
           builder: (controller) => Text(
             'Angka ${controller.count} ',
